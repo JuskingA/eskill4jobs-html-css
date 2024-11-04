9878f55dcea9b98b3b59b55f4257b8f8d3dc3522
JuskingA <juskingadjeinyarko@gmail.com>
2024-11-04 17:04:14 +0000

    comparison and logical operaters

--- Js/index.js
+++ Js/index.js
@@ -50,3 +50,82 @@ let b = undefined; // Undefined
 
 // let c = {}; // Object
 // let d = []; // Array
+
+// operaters in JavaScript
+// addtion
+// let result = 5 + 10;
+
+// subtraction
+// result = 5 - 10;
+
+// multiplcation
+// result = 5 * 10;
+
+// division
+// result = 5 / 10;
+
+// modulus
+// result = 5 % 100;
+
+// exponentiation
+// result = 5 ** 10;
+
+// increment
+// result = 5 % 10;
+// console.log(result);
+// let kofi = 10;
+// let Ama = 6;
+// let yaa = 5.50;
+// let total = kofi + Ama + yaa;
+// console.log('the total is'+ ' ' + total);
+
+// comparison operaters
+// let x = 10;
+// let y = 5;
+// let result = x > y;
+// console.log(result);
+
+// result = x < y;
+// console.log(result);
+
+// result = x >= y;
+// console.log(result);
+
+// result = x <= y;    
+// console.log(result);    
+
+// result = x == y;
+// console.log(result);
+
+// result = x != y;    
+// console.log(result);    
+
+// result = x === y;  
+// console.log(result);
+
+// result = x !== y;
+// console.log(result);
+
+// logical operaters
+let a = true;  
+let b = false;
+let c = true;
+let d = false;
+let result1 = a && b;
+console.log(result1);
+
+result1 = a || b;
+console.log(result1);
+
+result1 = !a;
+console.log(result1);
+
+result1 = !b;
+console.log(result1);
+
+result1 = !c;
+console.log(result1);
+
+result1 = !d;
+console.log(result1);
+
