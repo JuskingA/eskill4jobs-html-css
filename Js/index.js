a3b52e83536e1d0201c82d36188d3056dac9bb13
JuskingA <juskingadjeinyarko@gmail.com>
2024-11-04 15:44:31 +0000

    js class 2

---
+++ Js/index.js
@@ -0,0 +1,39 @@
+// console.log('Hello World');
+// console.log(5+10);
+// console.log('Hello' + 'World');
+// console.log('Hello' + 5);
+// var x = 15;
+// var y = 10;
+// console.log(x + y);
+// alert('connected');
+// alert('Hello World');
+
+// let a = 10;
+// let b = 5;
+// alert(a + b);
+
+// let c = 'Jusking';
+// let d = 'welcome';
+// alert(c +' '+ d);
+
+// let first_name = 'Jusking';
+// const last_name = 'Adjei Nyarko';
+// first_name = 'Kwame';
+// console.log(first_name +' '+ last_name);
+
+
+
+// const currentHour = new Date().getHours();
+// let greeting;
+
+// if (currentHour < 12) {
+//     greeting = 'Good Morning';
+// } else if (currentHour < 18) {
+//     greeting = 'Good Day'; 
+// } else {
+//     greeting = 'Good Evening'; 
+// }
+// alert(greeting);
+
+// $time = new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
+// alert($time);
