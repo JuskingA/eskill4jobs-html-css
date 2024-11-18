4599e4dd558cb495524bb003e43309888991945e
JuskingA <juskingadjeinyarko@gmail.com>
2024-11-18 15:55:12 +0000

    using the module

--- Js/nodejs/day1/fullname.js
+++ Js/nodejs/day1/fullname.js
@@ -1,17 +1,19 @@
 const firstName = 'Jusking'; // Variable declaration and initialization
 const lastName = 'Adjei Nyarko'; // Variable declaration and initialization
 const fullName = firstName + ' ' + lastName; // Concatenation of strings using the + operator
-console.log(fullName); // Output: Jusking Adjei Nyarko
+// console.log(fullName); // Output: Jusking Adjei Nyarko
 
-// or using template literals
-const firstName1 = 'Jusking'; // Variable declaration and initialization
-const lastName1 = 'Adjei Nyarko'; // Variable declaration and initialization
-const fullName1 = `${firstName1} ${lastName1}`; // Concatenation of strings using template literals
-console.log(fullName1); // Output: Jusking Adjei Nyarko
+// // or using template literals
+// const firstName1 = 'Jusking'; // Variable declaration and initialization
+// const lastName1 = 'Adjei Nyarko'; // Variable declaration and initialization
+// const fullName1 = `${firstName1} ${lastName1}`; // Concatenation of strings using template literals
+// console.log(fullName1); // Output: Jusking Adjei Nyarko
 
-// or using the + operator
-console.log('Jusking' + ' ' + 'Adjei Nyarko'); // Output: Jusking Adjei Nyarko
+// // or using the + operator
+// console.log('Jusking' + ' ' + 'Adjei Nyarko'); // Output: Jusking Adjei Nyarko
 
-// or using template literals
-console.log(`${'Jusking'} ${'Adjei Nyarko'}`); // Output: Jusking Adjei Nyarko
+// // or using template literals
+// console.log(`${'Jusking'} ${'Adjei Nyarko'}`); // Output: Jusking Adjei Nyarko
 
+// creating a module
+module.exports = fullName; // Exporting the fullName variable as a module
--- Js/nodejs/day1/index.js
+++ Js/nodejs/day1/index.js
@@ -1,1 +1,5 @@
-console.log('Jusking Adjei Nyarko');
+//console.log('Jusking Adjei Nyarko');
+
+// using the require function to import the fullName module
+const fullName = require('./fullname'); // Importing the fullName module
+console.log(fullName); // Output: Jusking Adjei Nyarko  // Importing the fullName module
