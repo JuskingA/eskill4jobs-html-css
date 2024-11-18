20ebaffdb53171859699b87c65f5113a0de04243
JuskingA <juskingadjeinyarko@gmail.com>
2024-11-18 15:43:44 +0000

    Activity one on NodeJS

---
+++ Js/nodejs/day1/fullname.js
@@ -0,0 +1,17 @@
+const firstName = 'Jusking'; // Variable declaration and initialization
+const lastName = 'Adjei Nyarko'; // Variable declaration and initialization
+const fullName = firstName + ' ' + lastName; // Concatenation of strings using the + operator
+console.log(fullName); // Output: Jusking Adjei Nyarko
+
+// or using template literals
+const firstName1 = 'Jusking'; // Variable declaration and initialization
+const lastName1 = 'Adjei Nyarko'; // Variable declaration and initialization
+const fullName1 = `${firstName1} ${lastName1}`; // Concatenation of strings using template literals
+console.log(fullName1); // Output: Jusking Adjei Nyarko
+
+// or using the + operator
+console.log('Jusking' + ' ' + 'Adjei Nyarko'); // Output: Jusking Adjei Nyarko
+
+// or using template literals
+console.log(`${'Jusking'} ${'Adjei Nyarko'}`); // Output: Jusking Adjei Nyarko
+
---
+++ Js/nodejs/day1/index.js
@@ -0,0 +1,1 @@
+console.log('Jusking Adjei Nyarko');
